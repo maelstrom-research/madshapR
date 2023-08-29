208e03b478e755a0b35da1847a1d746d47bb4f10
guifabre <guijoseph.fabre@gmail.com>
2023-08-29 14:19:07 -0400

    import from fabR functions for bookdown

---
+++ R/00-import-from-fabR.R
@@ -0,0 +1,13 @@
+
+#' @importFrom fabR bookdown_template
+#' @export
+fabR::bookdown_template
+
+#' @importFrom fabR bookdown_render
+#' @export
+fabR::bookdown_render
+
+#' @importFrom fabR bookdown_open
+#' @export
+fabR::bookdown_open
+
