f5623e6f4533591a34793f9a42cd448a5aa8da9d
guifabre <guijoseph.fabre@gmail.com>
2023-10-18 13:07:25 -0400

    export of functions from fabR to generate doc

--- R/00-import-from-fabR.R
+++ R/00-import-from-fabR.R
@@ -1,13 +1,21 @@
-
 #' @importFrom fabR bookdown_template
+#' @name bookdown_template
+#' @rdname bookdown_template
+#' @keywords exported
 #' @export
 fabR::bookdown_template
 
 #' @importFrom fabR bookdown_render
+#' @name bookdown_render
+#' @rdname bookdown_render
+#' @keywords exported
 #' @export
 fabR::bookdown_render
 
 #' @importFrom fabR bookdown_open
+#' @name bookdown_open
+#' @rdname bookdown_open
+#' @keywords exported
 #' @export
 fabR::bookdown_open
 
